33cf529992c598545fa3b3d537be39f45f1eda76
Everlasting12 <sidheshparab34@gmail.com>
2022-11-08 13:06:23 +0530

    added sharp and resize code

--- src/services/users/hooks/handleFileUpload.js
+++ src/services/users/hooks/handleFileUpload.js
@@ -1,5 +1,5 @@
 const multer = require('multer')
-
+const sharp = require('sharp')
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 
@@ -10,8 +10,9 @@ module.exports = function ()
         if (context.params.file)
         {
             const uploadedFiles = context.params.file;
+            const buffer = await sharp(uploadedFiles.buffer).resize({ width: 250, height: 250 }).toBuffer()
             context.data.profileImage = {
-                imageBuffer: uploadedFiles.buffer,
+                imageBuffer: buffer,
                 fileName: uploadedFiles.originalname + "-" + Date.now(),
                 type: uploadedFiles.mimetype,
             }
